25994c2aa4222d86aca1d37eaa44593ddc444f83
Коломійчук Віктор <555v.1.t.y.o.k555@gmail.com>
2016-01-11 20:08:01 +0200

    +

--- News/Global.asax.cs
+++ News/Global.asax.cs
@@ -14,7 +14,7 @@ namespace News
     {
         protected void Application_Start()
         {
-            //Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
+            Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
